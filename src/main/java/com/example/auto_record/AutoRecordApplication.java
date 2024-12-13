5cd020d6a34889636a87c637dfdd5cfb6cd46520
ipon <iphoneipon1207@gmail.com>
2024-12-13 21:22:05 +0900

    初期セットアップ

---
+++ src/main/java/com/example/auto_record/AutoRecordApplication.java
@@ -0,0 +1,13 @@
+package com.example.auto_record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/auto_record/AutoRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auto_record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
